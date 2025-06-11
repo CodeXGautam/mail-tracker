10bcd17499722f74913fba5044e89ba27c2040d9
Himanshu Sharma <himanshug1310@gmail.com>
2025-06-12 00:00:02 +0530

    refactor: update service worker to server.js and enhance tracking functionality

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ document.addEventListener("click", function (e) {
   if (e.target.innerText === "Send") {
     const composeBox = document.querySelector('[aria-label="Message Body"]');
     if (composeBox) {
-      const trackingId = "xyz123"; // Generate dynamically
+      const trackingId = `pixel${Math.random()}`; // Generate dynamically
       const trackingPixel = `<img src="http://localhost:4000/track/${trackingId}" width="1" height="1" />`;
-      console.log(trackingPixel)
+      const url = `http://localhost:4000/track/${trackingId}`
+      console.log(url)
       composeBox.innerHTML += trackingPixel;
     }
   }
